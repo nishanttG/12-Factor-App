f933060e03fa528bfc812fb5ebb99cf964831549
Nishant <ghimirenishant2060@gmail.com>
2025-05-15 17:38:58 +0545

    initial commit

--- _12factorapp/dataset.py
+++ _12factorapp/dataset.py
@@ -4,7 +4,7 @@ from loguru import logger
 from tqdm import tqdm
 import typer
 
-from 12factorapp.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
+from _12factorapp.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
 
 app = typer.Typer()
 
